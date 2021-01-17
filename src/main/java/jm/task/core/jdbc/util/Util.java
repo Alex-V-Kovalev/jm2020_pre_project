ad4dd08f765cac05179d3720bc0f0f6be08b60f0
Alex-V-Kovalev <a.kovalev@mail.ru>
2021-01-17 15:21:57 +0300

    pom.xml: Заменил dependency для подключения к postgresql Class Util: реализовал подключение к СУБД

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/jm2020_pre_project";
+    private static final String USER = "postgres";
+    private static final String PASS = "dbadmin";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager
+                        .getConnection(DB_URL, USER, PASS);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return connection;
+    }
+
 }
